25413f6cf67376dd97c3d3c7f9e90c949cd8f568
Gabriel Lichtenstein <gxl674@kif.ccs.miami.edu>
2022-09-19 15:45:04 -0400

    init main

---
+++ 01_run_test.sh
@@ -0,0 +1,8 @@
+# source: https://www.nextflow.io/docs/latest/getstarted.html
+
+#conda activate nextlfow;
+#nextflow run qbic-pipelines/cellranger -profile test,docker
+
+# check if nextflow is updated:
+# nextflow self-update
+
